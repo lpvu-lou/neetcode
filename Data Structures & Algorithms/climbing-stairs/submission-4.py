0e9d3e3790827da4eb338053cba2f1698dcb060f
lpvu-lou <phuongglou@gmail.com>
2026-07-08 21:33:53 +0200

    Add: climbing-stairs - submission-4

---
+++ Data Structures & Algorithms/climbing-stairs/submission-4.py
@@ -0,0 +1,11 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        # Dynamic Programming (Top-Down)
+        memo = {}
+        def helper(k):
+            if k <= 2:
+                return k
+            if k not in memo:
+                memo[k] = helper(k - 1) + helper(k - 2)
+            return memo[k]
+        return helper(n)
